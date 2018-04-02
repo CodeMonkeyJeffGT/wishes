bc4eb9f708f21d7092ed31096ad9656c83a90d81
codeMonkeyJeffGT <18846938586@163.com>
2018-04-02 18:17:17 +0800

    去除无用文件

--- Public/js/index.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $('#teacher').on('click', function () {
-        window.location.href="../1214/index.html"
-    })
-    $('#student').on('click', function () {
-        window.location.href="../1217/list.html"
-    })
-})
