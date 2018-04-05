d5605992ee54b7bbba903b32265a08b62b4439db
codeMonkeyJeffGT <18846938586@163.com>
2018-04-05 19:45:44 +0800

    debug

--- Application/V1/Model/UserModel.class.php
+++ Application/V1/Model/UserModel.class.php
@@ -76,7 +76,7 @@ class UserModel extends Model {
 				AND `angel_id` = %d
 		';
 		$user = $this->query($sql, $id);
-		$user['name'] = M()->db(1,"mysql://root:GT338570@localhost:3306/nefuer")->query("SELECT `name` FROM `user` WEHRE `acc` = " . $id);
+		$user['name'] = M()->db(1,"mysql://root:GT338570@localhost:3306/nefuer");
 		return $user;
 	}
 
