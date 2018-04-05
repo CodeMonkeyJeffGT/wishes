bf68d08ad7adcc40d262eac72653e8040e5fa823
codeMonkeyJeffGT <18846938586@163.com>
2018-04-05 19:32:32 +0800

    debug

--- Application/V1/Model/UserModel.class.php
+++ Application/V1/Model/UserModel.class.php
@@ -76,11 +76,7 @@ class UserModel extends Model {
 				AND `angel_id` = %d
 		';
 		$user = $this->query($sql, $id)[0];
-		$sql = '
-			USE `nefuer`;
-			SELECT `name` FROM `user` WEHRE `acc` = %d;
-		';
-		$user['name'] = $this->query($sql, $id)[0]['name'];
+		$user['name'] = $this->db(1,"mysql://root:GT338570@localhost:3306/nefuer")->query("SELECT `name` FROM `user` WEHRE `acc` = " . $id);
 		return $user;
 	}
 
